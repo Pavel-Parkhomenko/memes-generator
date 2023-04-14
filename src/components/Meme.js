a16083ee6e20ce23f27337e4b9cb985be218361d
Pavel Parkhomenka <pawelparhomenko@gmail.com>
2023-04-15 00:28:04 +0300

    add feature like download local meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import html2canvas from 'html2canvas';
 
 export default function Meme() {
   const [meme, setMeme] = React.useState({
     topText: "",
     bottomText: "",
-    url: "http://i.imgflip.com/1bij.jpg"
+    url: "http://i.imgflip.com/1bij.jpg",
   })
 
   const [allMemes, setAllMemes] = React.useState([])
+  const [status, setStatus] = React.useState("api") // types: local and api
 
   function handleChange(event) {
     const {name, value} = event.target
@@ -20,11 +22,12 @@ export default function Meme() {
 }
 
   function getMeme() {
+    setStatus("api")
     const randomNum = Math.floor(Math.random() * allMemes.length);
-    const meme = allMemes[randomNum].url;
+    const meme = allMemes[randomNum];
     setMeme(prevMeme => ({
         ...prevMeme,
-        url: meme
+        url: meme.url,
     }))
   }
 
@@ -34,6 +37,26 @@ export default function Meme() {
     .then(data => setAllMemes(data.data.memes))
   },[])
 
+  const handleBtnExport = () => {
+    html2canvas(document.querySelector('#meme')).then((canvas) => {
+      const img = canvas.toDataURL('image/jpeg');
+      const link = document.createElement('a');
+      const nameMeme = meme.url.split('/').at(-1).split('.')[0];
+      // console.log(nameMeme)
+      link.download = `meme-${nameMeme}.jpeg`;
+      link.href = img;
+      console.log(img)
+      link.click();
+    });
+  };
+
+  function onChangeInput(e) {
+    setStatus("local")
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      url: URL.createObjectURL(e.target.files[0]),
+    }))
+  }
 
   return (  
     <div className='main--container'>
@@ -41,12 +64,28 @@ export default function Meme() {
         <input type='text' placeholder='Top text' onChange={handleChange} name="bottomText"></input>
         <input type='text' placeholder='Bottom text' onChange={handleChange} name="topText"></input>
       </div>
-      <button onClick={getMeme}>Get a new meme image  🖼</button>
-      <div className='img--container'>
+      <div className="btn-input-container">
+        <input
+          className="input--file"
+          type="file"
+          accept=".png,.jpeg,.jpg"
+          onChange={(e) => onChangeInput(e)}
+        />
+        <button onClick={getMeme}>Get a new meme image  🖼</button>
+      </div>
+      <div id="meme" className='img--container'>
         <img src={meme.url} alt="" className='meme--img'></img>
         <p className='img--text top'>{meme.topText}</p>
         <p className='img--text bottom'>{meme.bottomText}</p>
       </div>
+      {status === "local"
+        ?
+        <button className="btn--download" onClick={handleBtnExport}>Download</button>
+        :
+        <button className="btn--download" disabled={true} onClick={handleBtnExport}>
+          Sorry, you cannot download using the API 😥
+        </button>
+      }
     </div>
   );
 }
